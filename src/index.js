033b5d13708efb718a1ca49517de6f48d10e4562
LCQ <lichaoqiangyes@163.com>
2019-05-29 21:47:05 +0800

    change version

--- src/index.js
+++ src/index.js
@@ -1,47 +1,84 @@
 /**
  * Created by LCQ on 2019-05-28,0028.
  */
-const AllModule = {
+class VueTools {
+    constructor (config) {
+        this.config = config
+    }
     // 验证手机号
     _checkPhone (val) {
-        try {
-            const reg = /^[1][3,4,5,7,8][0-9]{9}$/
-            if (typeof val !== 'number' && typeof val !== 'string') {
-                return false
-            }
-            if (!reg.test(val)) {
-                return false
-            }
-            return true
-        } catch (err) {
-            console.log(err)
+    try {
+        const reg = /^[1][3,4,5,7,8][0-9]{9}$/
+        if (typeof val !== 'number' && typeof val !== 'string') {
             return false
         }
-    },
-    // 验证邮箱
-    _checkEmail (val) {
-        try {
-            let reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-            if (typeof val !== 'string') {
-                return false
-            }
-            if (!reg.test(val)) {
-                return false
-            }
-            return true
-        } catch (err) {
-            console.log(err)
-            return 'error'
+        if (!reg.test(val)) {
+            return false
         }
+        return true
+    } catch (err) {
+        console.log(err)
+        return false
     }
 }
+    // 验证邮箱
+    _checkEmail (val) {
+    try {
+        let reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        if (typeof val !== 'string') {
+            return false
+        }
+        if (!reg.test(val)) {
+            return false
+        }
+        return true
+    } catch (err) {
+        console.log(err)
+        return 'error'
+    }
+}
+}
+// const AllModule = {
+//     // 验证手机号
+//     _checkPhone (val) {
+//         try {
+//             const reg = /^[1][3,4,5,7,8][0-9]{9}$/
+//             if (typeof val !== 'number' && typeof val !== 'string') {
+//                 return false
+//             }
+//             if (!reg.test(val)) {
+//                 return false
+//             }
+//             return true
+//         } catch (err) {
+//             console.log(err)
+//             return false
+//         }
+//     },
+//     // 验证邮箱
+//     _checkEmail (val) {
+//         try {
+//             let reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+//             if (typeof val !== 'string') {
+//                 return false
+//             }
+//             if (!reg.test(val)) {
+//                 return false
+//             }
+//             return true
+//         } catch (err) {
+//             console.log(err)
+//             return 'error'
+//         }
+//     }
+// }
 
 let config = {
     errorMsg: 'This method prohibits modification'
 }
 
 const genericInstall = (Vue) => {
-    Vue._VueTools = AllModule
+    Vue._VueTools = new VueTools()
     // Object.defineProperties(Vue.prototype, {
     //     _vueTools: {
     //         get () {
@@ -57,7 +94,7 @@ const genericInstall = (Vue) => {
 const _vueTools = {
     install (Vue, options) {
         if (options && options.name) {
-            Vue[options.name] = AllModule
+            Vue[options.name] = new VueTools()
             // Object.defineProperties(Vue.prototype, {
             //     [options.name]: {
             //         get () {
